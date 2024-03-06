e883a211a62958e652ac32de327b04835caebea8
DJ Mitchell <donald.mitchell@whatnot.com>
2024-03-06 11:53:54 -0500

    --wip-- [skip ci]

--- Source/SwiftLintBuiltInRules/Rules/Whatnot/LocaleOverrideRule.swift
+++ Source/SwiftLintBuiltInRules/Rules/Whatnot/LocaleOverrideRule.swift
@@ -1,4 +1,5 @@
 import SwiftSyntax
+import SwiftLintCore
 
 struct LocaleOverrideRule: ConfigurationProviderRule, SwiftSyntaxRule {
     var configuration = SeverityConfiguration(.error)
@@ -18,11 +19,16 @@ struct LocaleOverrideRule: ConfigurationProviderRule, SwiftSyntaxRule {
         triggeringExamples: [
             Example("let locale = ↓Locale.init()"),
             Example("let locale = ↓Locale()"),
+            Example("""
+                let locale = ↓Locale(identifier: "en_US")
+            """),
             Example("let locale = ↓Locale.current"),
             Example("↓Locale()"),
             Example("↓Locale.init()"),
             Example("↓Locale.current"),
-            Example("↓locale: .current"),
+            Example("""
+                Decimal(string: "123", ↓locale: .current)
+            """),
         ]
     )
 
@@ -31,26 +37,57 @@ struct LocaleOverrideRule: ConfigurationProviderRule, SwiftSyntaxRule {
     }
 }
 
-extension LocaleOverrideRule {
-    private final class Visitor: ViolationsSyntaxVisitor {
+private extension LocaleOverrideRule {
+    final class Visitor: ViolationsSyntaxVisitor {
         override func visitPost(_ node: MemberAccessExprSyntax) {
-            if let identifierExp = node.base?.as(IdentifierExprSyntax.self),
-               identifierExp.identifier.text == "Locale",
-               node.name.text != "overriddenOrCurrent" {
-                violations.append(node.positionAfterSkippingLeadingTrivia)
+            if isReferencingCurrentLocale(node) {
+                violations.append(reason(position: node.positionAfterSkippingLeadingTrivia))
             }
         }
 
         override func visitPost(_ node: FunctionCallExprSyntax) {
-            if let identifierExp = node.calledExpression.as(IdentifierExprSyntax.self),
-               identifierExp.identifier.text == "Locale",
-               node.argumentList.isEmpty {
-                violations.append(node.positionAfterSkippingLeadingTrivia)
-            } else if node.argumentList.contains(where: { element in
-                element.label?.text == "locale" && element.expression.as(MemberAccessExprSyntax.self)?.name.text == "current"
-            }) {
-                violations.append(node.positionAfterSkippingLeadingTrivia)
+            if isLocaleInitializer(node) {
+                violations.append(reason(position: node.positionAfterSkippingLeadingTrivia))
+            } else if let localeArgument = currentLocaleAsFunctionArgument(node) {
+                violations.append(reason(position: localeArgument.positionAfterSkippingLeadingTrivia))
             }
         }
+    }
+}
+
+private extension LocaleOverrideRule.Visitor {
+    func isReferencingCurrentLocale(_ node: MemberAccessExprSyntax) -> Bool {
+        if let identifierExp = node.base?.as(IdentifierExprSyntax.self),
+           identifierExp.identifier.text == "Locale",
+           node.name.text == "current" {
+            return true
+        }
+
+        return false
+    }
+
+    func isLocaleInitializer(_ node: FunctionCallExprSyntax) -> Bool {
+        if let identifierExp = node.calledExpression.as(IdentifierExprSyntax.self),
+           identifierExp.identifier.text == "Locale" {
+            return true
+        }
+
+        return false
+    }
+
+    func currentLocaleAsFunctionArgument(_ node: FunctionCallExprSyntax) -> TupleExprElementListSyntax.Element? {
+        node.argumentList.first(where: { element in
+            element.label?.text == "locale" && element.expression.as(MemberAccessExprSyntax.self)?.name.text == "current"
+        })
+    }
+}
+
+private extension LocaleOverrideRule.Visitor {
+    func reason(position: AbsolutePosition) -> ReasonedRuleViolation {
+        .init(
+            position: position,
+            reason: "Locale.overriddenOrCurrent allows us to override the locale for testing purposes; prefer it over other instances of Locale",
+            severity: .warning
+        )
     }
 }
