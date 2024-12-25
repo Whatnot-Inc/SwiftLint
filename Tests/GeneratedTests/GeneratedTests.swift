288139a150a8778f27e8d0aaa3a77bd2f615d0de
Danny Mösch <danny.moesch@icloud.com>
2024-12-25 23:05:02 +0100

    Add new `redundant_sendable` rule (#5902)

--- Tests/GeneratedTests/GeneratedTests.swift
+++ Tests/GeneratedTests/GeneratedTests.swift
@@ -1063,6 +1063,12 @@ final class RedundantSelfInClosureRuleGeneratedTests: SwiftLintTestCase {
     }
 }
 
+final class RedundantSendableRuleGeneratedTests: SwiftLintTestCase {
+    func testWithDefaultConfiguration() {
+        verifyRule(RedundantSendableRule.description)
+    }
+}
+
 final class RedundantSetAccessControlRuleGeneratedTests: SwiftLintTestCase {
     func testWithDefaultConfiguration() {
         verifyRule(RedundantSetAccessControlRule.description)
