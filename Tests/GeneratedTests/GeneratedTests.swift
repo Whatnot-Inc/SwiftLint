e1834ffb6b8a787c2e30bec21af8a8704096e74e
Danny Mösch <danny.moesch@icloud.com>
2025-02-12 22:46:28 +0100

    Add new `opaque_over_existential` rule (#5915)

--- Tests/GeneratedTests/GeneratedTests.swift
+++ Tests/GeneratedTests/GeneratedTests.swift
@@ -841,6 +841,12 @@ final class OneDeclarationPerFileRuleGeneratedTests: SwiftLintTestCase {
     }
 }
 
+final class OpaqueOverExistentialParameterRuleGeneratedTests: SwiftLintTestCase {
+    func testWithDefaultConfiguration() {
+        verifyRule(OpaqueOverExistentialParameterRule.description)
+    }
+}
+
 final class OpeningBraceRuleGeneratedTests: SwiftLintTestCase {
     func testWithDefaultConfiguration() {
         verifyRule(OpeningBraceRule.description)
